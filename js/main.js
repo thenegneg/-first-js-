50ccd7a0143091c194c36db83e2f691f382906fa
thenegneg <bluemoonrinna@gmail.com>
2023-01-21 20:16:11 +0330

    js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+console.log("hello-world")
+
+alert("hello everyone! :D")
+
+document.write("<div class='bg01'> <span class='text01'>A different view on beauty</span> </div>")
+
+document.querySelector("div#root").innerHTML=("<div class='bg02'> <span class='text02'>Because we are from Sweden we have a different – and holistic – view on beauty. It’s not only <br/> about how you look, but how you live, feel and act. To us beauty is a way of life: to be healthy, <br/> enjoy beautiful skin, express yourself and have fun. That’s beauty by Sweden – our way of helping <br/> you live a healthier, more beautiful life.!</span> </div>")
+
+var New="Passion Fruit Multi-purpose Balm"
+var offer="Subzero Hair & Body Wash"
+var Healthy ="Meal Replacement Scoop"
+document.write(`<div class='flex'> <div class='var'> <p class='tittle'>What's new ? <span>${New}</span></p> <br/> <p class='tittle'>Best offers ! <span>${offer}</span></p> <br/> <p class='tittle'>Healthy weight ! <span>${Healthy}</span></p> <br/> <img class='img' src="./03.jfif"> </div> </div>`)
+
+let x=200
+console.log(x)
+console.log(typeof x)
+
+let y="hi"
+console.log(y)
+console.log(typeof y)
+
+let z
+console.log(z)
+console.log(typeof z)
+
+let w=null
+console.log(w)
+console.log(typeof w)
+
+let t=true
+console.log(t)
+console.log(typeof t)
+
+let f=false
+console.log(f)
+console.log(typeof f)
+
+const pi=3.14
+console.log(pi)
+console.log(typeof pi)
+
+let Name=prompt("hi im negneg :D please tell me your name...!")
+let age=prompt("and your age...!")
+let gender=prompt("goood... whats your gender...?")
+let email=prompt("please write me your email...!         *thankful*")
+
+console.log(`my name is ${Name}\n and my age is ${age}\n and my gender is ${gender}\n and my email is ${email}`)
+
+document.write(`<div class='var text03'> my name is : <p> ${Name} </p>  \t and my age is : <p> ${age} </p>  \t and my gender is : <p> ${gender} </p>  \t and my email : is <p> ${email} </p> </div>`)
+
